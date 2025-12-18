fca96e720e12c1227aa4b12f720688f3c07cc929
Magdalena <magdalenapersson@live.se>
2025-12-18 11:54:01 +0100

    added vis2

---
+++ data/vis2.sql
@@ -0,0 +1,16 @@
+SELECT * FROM Production.ProductCategory
+SELECT * FROM Production.ProductSubcategory
+SELECT * FROM Production.Product
+SELECT * FROM Sales.SalesOrderDetail
+
+
+SELECT
+    pc.Name AS CategoryName,
+    SUM(sod.LineTotal) AS TotalRevenue
+FROM Sales.SalesOrderDetail sod
+INNER JOIN Production.Product p ON sod.ProductID = p.ProductID
+INNER JOIN Production.ProductSubcategory psc ON p.ProductSubcategoryID = psc.ProductSubcategoryID
+INNER JOIN Production.ProductCategory pc ON psc.ProductCategoryID = pc.ProductCategoryID
+GROUP BY pc.Name
+ORDER BY TotalRevenue ASC;
+
