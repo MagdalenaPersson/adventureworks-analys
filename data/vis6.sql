5582372210f3cbb5d38e855d0e8556d6298982b9
Magdalena <magdalenapersson@live.se>
2025-12-22 22:29:14 +0100

    added vis6

---
+++ data/vis6.sql
@@ -0,0 +1,13 @@
+SELECT * FROM Sales.SalesTerritory
+SELECT * FROM Sales.SalesOrderHeader
+SELECT * FROM Sales.Customer
+
+SELECT 
+    st.Name AS Region,
+    SUM(soh.TotalDue) AS TotalSales,
+    COUNT(DISTINCT c.CustomerID) AS Customers
+FROM Sales.SalesTerritory st
+INNER JOIN Sales.SalesOrderHeader soh ON st.TerritoryID = soh.TerritoryID
+INNER JOIN Sales.Customer c ON soh.CustomerID = c.CustomerID
+GROUP BY st.Name
+ORDER BY TotalSales DESC
