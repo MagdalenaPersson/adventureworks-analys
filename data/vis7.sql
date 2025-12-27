befe1ef3f5c47ca35d970a6d6210febfc1b6c242
Magdalena <magdalenapersson@live.se>
2025-12-27 22:15:03 +0100

    added vis7

---
+++ data/vis7.sql
@@ -0,0 +1,22 @@
+SELECT * FROM Sales.SalesTerritory 
+SELECT * FROM Sales.SalesOrderHeader
+SELECT * FROM Sales.Customer
+SELECT * FROM Sales.Store
+
+
+SELECT
+    st.Name AS Region,
+    CASE
+        WHEN c.StoreID IS NOT NULL THEN 'Store'
+        ELSE 'Individual'
+    END AS CustomerType,
+    SUM(soh.TotalDue) / COUNT(soh.SalesOrderID) AS AverageOrderValue
+FROM Sales.SalesOrderHeader soh
+INNER JOIN Sales.SalesTerritory st ON soh.TerritoryID = st.TerritoryID
+INNER JOIN Sales.Customer c ON soh.CustomerID = c.CustomerID
+LEFT JOIN Sales.Store s ON c.StoreID = s.BusinessEntityID
+GROUP BY st.Name,
+    CASE 
+        WHEN c.StoreID IS NOT NULL THEN 'Store'
+        ELSE 'Individual'
+    END
